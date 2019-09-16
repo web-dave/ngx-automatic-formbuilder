8efccbf5166a408e782fd3f89adc0bf01b0be869
Dave <dave@webdave.de>
2019-09-16 21:34:02 +0200

    playground lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  IFormSettings,
+  DynamicFormBuilder
+} from 'projects/ngx-dynamic-formbuilder/src/public-api';
+// import { DynamicFormBuilder, IFormSettings } from './dyn-form-builder.class';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formbuilder';
+  myForm: FormGroup;
+  dynKeys;
+  settings: IFormSettings = {
+    title: [
+      '',
+      [Validators.required],
+      { type: 'text', errormsg: { required: 'Title is required!' } }
+    ],
+    subtitle: ['', []],
+    isbn: ['', []],
+    abstract: ['', []],
+    numPages: [0, []],
+    author: ['', []],
+    publisher: {
+      name: ['', []],
+      url: ['', []],
+      address: {
+        street: [''],
+        city: [''],
+        zip: ['']
+      }
+    }
+  };
+  constructor(private builder: DynamicFormBuilder) {
+    this.myForm = builder.setupForm(this.settings);
+    this.dynKeys = Object.keys(this.myForm.controls);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxDynamicFormbuilderModule } from 'projects/ngx-dynamic-formbuilder/src/public-api';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgxDynamicFormbuilderModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
